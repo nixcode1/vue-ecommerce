8dac2f6ff066c7cf82490e43626aedf3b3f08469
Amaechi Chukwuebuka <amaechi.chukwuebuka1@gmail.com>
2020-11-25 12:42:33 +0100

    before upgrade to vue3

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="scrolling-techniques-7">
-      <div class="d-flex flex-row flex-wrap flex-shrink align-center">
-        <v-card v-for="product in products" :key="product.id" class="mx-4 my-2 pa-2" height="400" max-width="250">
-        <v-img :src="product.imageUrl" height="200px" contain></v-img>
-        <v-card-title class="pa-md-6"> {{ product.name }} </v-card-title>
+    <div class="d-flex flex-row flex-wrap flex-shrink justify-space-around">
+      <v-card
+        v-for="product in products"
+        :key="product.id"
+        class="mx-4 my-2 pa-2"
+        height="200"
+        width="200"
+      >
+        <v-img :src="product.imageUrl" height="100px" contain></v-img>
+        <v-card-title>
+          <div class="headerClass">{{ product.name }}</div>
+        </v-card-title>
       </v-card>
-      </div>
+    </div>
   </div>
 </template>
 
@@ -26,5 +34,11 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.headerClass {
+  white-space: nowrap;
+  word-break: normal;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
 </style>
