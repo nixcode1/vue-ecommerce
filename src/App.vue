840f3834c7d50aece381e76c6b3fd5fd4cef9f19
Amaechi Chukwuebuka <amaechi.chukwuebuka1@gmail.com>
2020-11-25 20:38:08 +0100

    added vue-router and vuex

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,78 @@
 <template>
-  <v-app>
-    <v-app-bar app color="white" elevate-on-scroll>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <div id="app">
+    <v-app>
+      <v-navigation-drawer app>
+        <!-- -->
+      </v-navigation-drawer>
 
-      <v-toolbar-title>Title</v-toolbar-title>
+      <v-app-bar
+        app
+        color="#fcb69f"
+        dark
+        prominent
+        shrink-on-scroll
+        src="https://picsum.photos/1920/1080?random"
+      >
+        <template v-slot:img="{ props }">
+          <v-img
+            v-bind="props"
+            gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+          ></v-img>
+        </template>
 
-      <v-spacer></v-spacer>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+        <v-toolbar-title>Title</v-toolbar-title>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
+        <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-app-bar>
+        <router-link to="/">
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+        </router-link>
 
-    <v-main>
-      <homepage />
-    </v-main>
-  </v-app>
+        <router-link to="/about">
+          <v-btn icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-btn>
+        </router-link>
+        <router-link to="/dashboard"
+          ><v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn></router-link
+        >
+      </v-app-bar>
+
+      <!-- Sizes your content based upon application components -->
+      <v-main app>
+        <!-- Provides the application the proper gutter -->
+        <v-container fluid>
+          <!-- If using vue-router -->
+          <router-view></router-view>
+
+          <v-container
+            style="height: 1000px"
+          ></v-container>
+        </v-container>
+      </v-main>
+    </v-app>
+  </div>
 </template>
 
+|
+      
+
 <script>
-// import HelloWorld from "./components/HelloWorld";
-// import Dashboard from './components/Dashboard';
-import Homepage from "./components/Homepage.vue";
-
-export default {
-  name: "App",
-
-  components: {
-    // Dashboard,
-    Homepage,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+export default {};
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import { firestorePlugin } from 'vuefire'
+import router from './router'
+import store from './store'
 
 Vue.use(firestorePlugin)
 
@@ -9,5 +11,7 @@ Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  router,
+  store,
   render: h => h(App)
 }).$mount('#app')
